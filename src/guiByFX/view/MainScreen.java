62d13a084a0f6513137d00c65f1df15c407d3de0
VladimirDoneth <vovan.s.marsa@gmail.com>
2018-05-15 00:40:14 +0300

    This class is controller for main screen

--- src/guiByFX/view/MainScreen.java
+++ src/guiByFX/view/MainScreen.java
@@ -1,4 +1,37 @@
 package guiByFX.view;
 
+import guiByFX.Main;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class MainScreen {
+
+    /**
+     * method is handler for clicking exit button
+     * @param actionEvent
+     */
+    public void doExitButton(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void doCreateModel(MouseEvent mouseEvent) {
+        System.out.println("createModel");
+    }
+
+    public void doStartModeling(MouseEvent mouseEvent) {
+        System.out.println("startModeling");
+    }
+
+    public void doRollupButton(ActionEvent actionEvent) {
+        Main.primaryStage.setIconified(true);
+    }
+
+
+    public void doMoreButton(ActionEvent actionEvent) {
+        System.out.println("more");
+    }
+
+    public void doMovingByMouse(MouseEvent mouseEvent) {
+        System.out.println("move"); ///не знаю пока как двигать панель
+    }
 }
