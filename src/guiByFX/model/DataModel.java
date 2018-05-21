6e1f4c5d08f29c61e8bca5cc197387cf73dbf3d9
VladimirDoneth <vovan.s.marsa@gmail.com>
2018-05-21 12:43:02 +0300

    This class represnts part of MVC-pattern model

--- src/guiByFX/model/DataModel.java
+++ src/guiByFX/model/DataModel.java
@@ -1,4 +1,43 @@
 package guiByFX.model;
 
+import geneticAlgirithmCore.BasicInfo;
+
 public class DataModel {
+    private String aboutModel;
+    private String dateCreate;
+    private BasicInfo basicInfo;
+
+    public DataModel() {
+        basicInfo = new BasicInfo();
+    }
+
+    public DataModel(String aboutModel, String dateCreate, BasicInfo basicInfo) {
+        this.aboutModel = aboutModel;
+        this.dateCreate = dateCreate;
+        this.basicInfo = basicInfo;
+    }
+
+    public BasicInfo getBasicInfo() {
+        return basicInfo;
+    }
+
+    public String getAboutModel() {
+        return aboutModel;
+    }
+
+    public String getDateCreate() {
+        return dateCreate;
+    }
+
+    public void setAboutModel(String aboutModel) {
+        this.aboutModel = aboutModel;
+    }
+
+    public void setDateCreate(String dateCreate) {
+        this.dateCreate = dateCreate;
+    }
+
+    public void setBasicInfo(BasicInfo basicInfo) {
+        this.basicInfo = basicInfo;
+    }
 }
