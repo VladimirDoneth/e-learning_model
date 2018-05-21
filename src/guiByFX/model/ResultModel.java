3cf579612c6434c390ea9291c7aa49bfe756708e
VladimirDoneth <vovan.s.marsa@gmail.com>
2018-05-21 12:43:53 +0300

    This class represents part of MVC-pattern model

--- src/guiByFX/model/ResultModel.java
+++ src/guiByFX/model/ResultModel.java
@@ -1,4 +1,47 @@
 package guiByFX.model;
 
+import geneticAlgirithmCore.GenomeOfAgent;
+
 public class ResultModel {
+    private int idModel; //??? I don`t now, I need this method or no
+    private String commentResult;
+    private GenomeOfAgent agent;
+
+    public ResultModel(int idModel) {
+        this.idModel = idModel;
+    }
+
+    public ResultModel() {
+
+    }
+
+    public ResultModel(int idModel, String commentResult, GenomeOfAgent agent) {
+        this.idModel = idModel;
+        this.commentResult = commentResult;
+        this.agent = agent;
+    }
+
+    public void setAgent(GenomeOfAgent agent) {
+        this.agent = agent;
+    }
+
+    public void setCommentResult(String commentResult) {
+        this.commentResult = commentResult;
+    }
+
+    public void setIdModel(int idModel) {
+        this.idModel = idModel;
+    }
+
+    public GenomeOfAgent getAgent() {
+        return agent;
+    }
+
+    public int getIdModel() {
+        return idModel;
+    }
+
+    public String getCommentResult() {
+        return commentResult;
+    }
 }
