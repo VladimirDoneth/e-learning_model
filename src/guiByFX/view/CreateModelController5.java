c4848aa2d1d1b80ebbb26772c4a443b5c0aee58b
VladimirDoneth <vovan.s.marsa@gmail.com>
2018-05-21 21:21:32 +0300

    class controller for crete_model_step5.java

--- src/guiByFX/view/CreateModelController5.java
+++ src/guiByFX/view/CreateModelController5.java
@@ -1,4 +1,36 @@
 package guiByFX.view;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+
 public class CreateModelController5 {
+    public static int usedAppInt[];
+    public static int usedDimInt[];
+
+    public ScrollPane bAppScrollPane;
+    public ScrollPane bDimScrollPane;
+    public Label myIndexation;
+    public Label myTrsIndexation;
+
+    public void doActivateFields(MouseEvent mouseEvent) {
+
+    }
+
+    public void doNextApp(MouseEvent mouseEvent) {
+
+    }
+
+    public void doCancel(ActionEvent actionEvent) {
+
+    }
+
+    public void doNextTask(ActionEvent actionEvent) {
+
+    }
+
+    public void doPerviousApp(MouseEvent mouseEvent) {
+
+    }
 }
