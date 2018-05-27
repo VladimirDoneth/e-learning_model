4762077f645fae34772ae22c18c31452e84b51ae
VladimirDoneth <vovan.s.marsa@gmail.com>
2018-05-27 14:56:14 +0300

    add method for fix some bugs

--- src/guiByFX/view/MainScreen.java
+++ src/guiByFX/view/MainScreen.java
@@ -1,7 +1,10 @@
 package guiByFX.view;
 
+import dataBaseCore.DBCore;
 import guiByFX.Main;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -11,8 +14,15 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MainScreen {
+
+    @FXML
+    private void initialize(){
+        //позже допишу
+    }
 
     /**
      * method is handler for clicking exit button
@@ -20,6 +30,30 @@ public class MainScreen {
      */
     public void doExitButton(ActionEvent actionEvent) {
         System.exit(0);
+    }
+
+    /**
+     * method is handler for button of 'start modeling', if it clicked main_screen not hide
+     * and start screen 'modeling_screen_start.fxml' and user can do modeling process
+     * @param mouseEvent
+     */
+    public void doStartModeling(MouseEvent mouseEvent) throws IOException, SQLException, ClassNotFoundException {
+        Stage childStage = new Stage();
+        childStage.setAlwaysOnTop(false);
+        childStage.initStyle(StageStyle.UTILITY);
+
+        ModelingScreenController.models = FXCollections.observableArrayList();
+        DBCore dbCore = new DBCore(null, DBCore.DB_NAME);
+        ArrayList<DBCore.SimpleModel> simpleModels = dbCore.getSimpleModels();
+        dbCore.closeConnection();
+        for (int i = 0; i < simpleModels.size(); i++) {
+            ModelingScreenController.models.add(i, simpleModels.get(i).gerateStringForComboBox());
+        }
+
+        Parent root = FXMLLoader.load(getClass().getResource("modeling_screen_start.fxml"));
+        childStage.setScene(new Scene(root, 640, 400));
+        childStage.initOwner((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+        childStage.showAndWait();
     }
 
     /**
@@ -34,10 +68,6 @@ public class MainScreen {
         Parent root = FXMLLoader.load(getClass().getResource("create_model_step1.fxml"));
         mainStage.setScene(new Scene(root, 640, 300));
         mainStage.show();
-    }
-
-    public void doStartModeling(MouseEvent mouseEvent) {
-        System.out.println("startModeling");
     }
 
     public void doRollupButton(ActionEvent actionEvent) {
